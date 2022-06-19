79f80fdac0f1da2a4a0c4e9a696df4b3986138b9
gehll <99802140+gehll@users.noreply.github.com>
2022-06-19 18:04:11 +0200

    Update funcion1.R

--- funcion1.R
+++ funcion1.R
@@ -1,6 +1,8 @@
 # Primer script
 # Vamos a hacer unos cambios y mandarlos al remoto
+#CAMBIOS EN RAMA2!!!
 funcion <- function(a, b, c, coef){
   d <- coef*(c+b)
-  return(a*d)
+  z <- d*1.25
+  return(a*z)
 }
